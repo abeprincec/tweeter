86bffe047f68e02bf386af5554568a14fdfaec37
abeprincec <abeprincec@gmail.com>
2018-05-23 13:58:52 +0000

    HTML string for tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,59 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+	$('.new-tweet-form').submit(function(event) {
+		event.preventDefault();
+		$(this).serialize();
+	});
 
+	function loadTweets() {
+		$.ajax({
+			url: '/tweets',
+			method: 'GET',
+			success: function(jsonData) {
+				renderTweets(jsonData);
+			},
+		});
+	}
+
+	loadTweets();
+
+	function createTweetElement(tweet) {
+		//		const $tweet = $('<article>').addClass('tweet');
+
+		let $tweet = ` <article class="tweet"><header class="tweet-header">
+        <img src="${tweet.user.avatars.small}"> </img>
+        <h2 class="tweet-name">${tweet.user.name}</h2>
+        <p class="tweet-handler">${tweet.user.handle}</p>
+        
+      </header>
+      <div class="tweet-msg-container">
+      <p class="tweet-message">${tweet.content.text}</p>
+    </div>
+    <hr>
+    <footer>
+      <p>${tweet.created_at}</p>
+      <div class="footer-icons">
+        <i class="fab fa-font-awesome-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+    </article>`;
+
+		//return $('.tweets-container').append($tweet);
+		return $tweet;
+	}
+
+	//console.log(createTweetElement(data.users));
+
+	function renderTweets(data) {
+		return data.map(elem => {
+			let $tweet = createTweetElement(elem);
+			$('.tweets-container').append($tweet);
+		});
+	}
+
+	
+});
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	// --- our code goes here ---
+
+	$('textarea').on('input', function(event) {
+		const valuelen = $(this).val().length;
+		const maxlength = 140;
+		let length = maxlength - valuelen;
+		$(this)
+			.siblings('.counter')
+			.text(length);
+
+		length < 0
+			? $(this)
+					.siblings('.counter')
+					.addClass('exceeded')
+			: $(this)
+					.siblings('.counter')
+					.removeClass('exceeded');
+	});
+});
