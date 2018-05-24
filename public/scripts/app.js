46726fd84c2b6f2c629da0fe157cd9a3143ad4a7
abeprincec <abeprincec@gmail.com>
2018-05-24 15:53:17 +0000

    implemeted single tweet ajax function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,11 +4,56 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-	$('.new-tweet-form').submit(function(event) {
-		event.preventDefault();
-		$(this).serialize();
+	function escape(str) {
+		var div = document.createElement('div');
+		div.appendChild(document.createTextNode(str));
+		return div.innerHTML;
+	}
+	//Compose tweet hide and show animation
+	$('.new-tweet').hide();
+
+	$('.header-button').click(function(event) {
+		$('.new-tweet').toggle(300);
+		$('#composetweet').focus();
 	});
 
+	//form submit -> form validation, ajax post method for tweet and get last tweet.
+	$('.new-tweet-form').submit(function(event) {
+		event.preventDefault();
+		var formInput = $(this).serialize();
+
+		console.log($('#composetweet').val().length);
+
+		if (!$('#composetweet').val()) {
+			console.log('please enter an input');
+		} else if ($('#composetweet').val().length < 140) {
+			$.ajax({
+				url: '/tweets',
+				method: 'POST',
+				data: formInput,
+
+				success: function(formInput) {
+					renderNewTweet(formInput);
+					$('#composetweet').val('');
+				},
+			});
+		}
+	});
+
+	//render tweet after composing tweet
+	function renderNewTweet(data) {
+		$.ajax({
+			url: '/tweets',
+			method: 'GET',
+			success: function(jsonData) {
+				let lastTweet = createTweetElement(jsonData.slice(-1).pop());
+
+				$('.tweets-container').prepend(lastTweet);
+			},
+		});
+	}
+
+	//loadTweet using json data from /tweets
 	function loadTweets() {
 		$.ajax({
 			url: '/tweets',
@@ -21,9 +66,9 @@ $(document).ready(function() {
 
 	loadTweets();
 
+	//create one tweet element
 	function createTweetElement(tweet) {
 		//		const $tweet = $('<article>').addClass('tweet');
-
 		let $tweet = ` <article class="tweet"><header class="tweet-header">
         <img src="${tweet.user.avatars.small}"> </img>
         <h2 class="tweet-name">${tweet.user.name}</h2>
@@ -31,7 +76,7 @@ $(document).ready(function() {
         
       </header>
       <div class="tweet-msg-container">
-      <p class="tweet-message">${tweet.content.text}</p>
+      <p class="tweet-message">${escape(tweet.content.text)}</p>
     </div>
     <hr>
     <footer>
@@ -43,19 +88,14 @@ $(document).ready(function() {
       </div>
     </footer>
     </article>`;
-
-		//return $('.tweets-container').append($tweet);
 		return $tweet;
 	}
 
-	//console.log(createTweetElement(data.users));
-
+	//
 	function renderTweets(data) {
 		return data.map(elem => {
 			let $tweet = createTweetElement(elem);
-			$('.tweets-container').append($tweet);
+			$('.tweets-container').prepend($tweet);
 		});
 	}
-
-	
 });
